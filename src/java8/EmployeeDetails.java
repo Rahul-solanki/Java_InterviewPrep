4f440061491a7ead466616c3acc1c4682197eda1
Rahul-solanki <rahulsolanki1227@gmail.com>
2024-01-02 09:16:59 +0530

    first commit.

---
+++ src/java8/EmployeeDetails.java
@@ -0,0 +1,7 @@
+package java8;
+
+public class EmployeeDetails {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/java8/StreamsTest.java
@@ -0,0 +1,7 @@
+package java8;
+
+public class StreamsTest {
+    public static void main(String[] args) {
+
+    }
+}
